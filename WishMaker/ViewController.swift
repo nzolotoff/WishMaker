fb745c0bda0502bda1859d247c81024e9bfb2655
nzolotoff <nikita.zolotov37@yandex.ru>
2024-11-03 00:13:51 +0300

    initial commit

---
+++ WishMaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WishMaker
+//
+//  Created by Nikita Zolotov on 03.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
