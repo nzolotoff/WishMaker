86774558e8e61cb333bb13b9e1bb76e94c081faf
nzolotoff <nikita.zolotov37@yandex.ru>
2024-11-21 20:40:14 +0300

    create table for wishes

--- WishMaker/Controllers/WishStoringViewController.swift
+++ WishMaker/Controllers/WishStoringViewController.swift
@@ -8,15 +8,45 @@
 import UIKit
 
 final class WishStoringViewController: UIViewController {
+    // MARK: - Fields
+    private let wishesTable: UITableView = UITableView(frame: .zero)
 
+    // MARK: - Lyfecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
-
-        // Do any additional setup after loading the view.
+        configureUI()
     }
     
+    // MARK: - Private methods
+    private func configureUI() {
+        view.backgroundColor = .cyan
+        configureWishesTable()
+    }
     
-    
+    private func configureWishesTable() {
+        wishesTable.backgroundColor = .red
+        wishesTable.separatorStyle = .none
+        wishesTable.dataSource = self
+        wishesTable.layer.cornerRadius = 20
+        wishesTable.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(wishesTable)
+        NSLayoutConstraint.activate([
+            wishesTable.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            wishesTable.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            wishesTable.heightAnchor.constraint(equalToConstant: 500),
+            wishesTable.widthAnchor.constraint(equalToConstant: 300)
+            ])
+    }
+}
 
+// MARK: UITableViewDataSource
+extension WishStoringViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
 }
