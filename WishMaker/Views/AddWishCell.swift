c9386faf841d3446f152f7b2ed9d415cec6ba3a3
nzolotoff <nikita.zolotov37@yandex.ru>
2024-11-24 15:00:49 +0300

    get rid of magic numbers

--- WishMaker/Views/AddWishCell.swift
+++ WishMaker/Views/AddWishCell.swift
@@ -8,6 +8,26 @@
 import UIKit
 
 final class AddWishCell: UITableViewCell {
+    // MARK: - Constants
+    enum Constants {
+        static let textViewCorner: CGFloat = 8
+        static let textViewTextColor: UIColor = .systemPink
+        static let textViewFontSize: CGFloat = 16
+        static let textViewBorderWidth: CGFloat = 0.5
+        
+        static let addWishButtonImageName: String = "sparkles"
+        
+        static let textViewLeadingOffset: CGFloat = 10
+        static let textViewTrailingOffset: CGFloat = -1 * 50
+        static let textViewTopOffset: CGFloat = 10
+        static let textViewHeight: CGFloat = 60
+        
+        static let addWishButtonLeadingOffset: CGFloat = 10
+        static let addWishButtonTopOffset: CGFloat = 25
+        static let addWishButtonWidth: CGFloat = 30
+        static let addWishButtonHeight: CGFloat = 30
+    }
+    
     // MARK: - Static Fields
     static let reuseId: String = "AddWishCell"
     
@@ -37,24 +57,25 @@ final class AddWishCell: UITableViewCell {
         [addWishTextView, addWishButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             addSubview($0)}
-        addWishButton.addTarget(self, action: #selector(addWishButtonTapped), for: .touchUpInside)
-        addWishTextView.layer.cornerRadius = 8
-        addWishTextView.textColor = .systemPink
-        addWishTextView.font = .systemFont(ofSize: 16)
-        addWishTextView.layer.borderWidth = 0.5
         
-        addWishButton.setImage((UIImage(systemName: "sparkles")), for: .normal)
+        addWishTextView.layer.cornerRadius = Constants.textViewCorner
+        addWishTextView.textColor = Constants.textViewTextColor
+        addWishTextView.font = .systemFont(ofSize: Constants.textViewFontSize)
+        addWishTextView.layer.borderWidth = Constants.textViewBorderWidth
+        
+        addWishButton.setImage((UIImage(systemName: Constants.addWishButtonImageName)), for: .normal)
+        addWishButton.addTarget(self, action: #selector(addWishButtonTapped), for: .touchUpInside)
         
         NSLayoutConstraint.activate([
-            addWishTextView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
-            addWishTextView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -50),
-            addWishTextView.topAnchor.constraint(equalTo: topAnchor, constant: 5),
-            addWishTextView.heightAnchor.constraint(equalToConstant: 60),
+            addWishTextView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.textViewLeadingOffset),
+            addWishTextView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: Constants.textViewTrailingOffset),
+            addWishTextView.topAnchor.constraint(equalTo: topAnchor, constant: Constants.textViewTopOffset),
+            addWishTextView.heightAnchor.constraint(equalToConstant: Constants.textViewHeight),
             
-            addWishButton.leadingAnchor.constraint(equalTo: addWishTextView.trailingAnchor, constant: 10),
-            addWishButton.topAnchor.constraint(equalTo: contentView.centerYAnchor),
-            addWishButton.widthAnchor.constraint(equalToConstant: 30),
-            addWishButton.heightAnchor.constraint(equalToConstant: 30)
+            addWishButton.leadingAnchor.constraint(equalTo: addWishTextView.trailingAnchor, constant: Constants.addWishButtonLeadingOffset),
+            addWishButton.topAnchor.constraint(equalTo: topAnchor, constant: Constants.addWishButtonTopOffset),
+            addWishButton.widthAnchor.constraint(equalToConstant: Constants.addWishButtonWidth),
+            addWishButton.heightAnchor.constraint(equalToConstant: Constants.addWishButtonHeight)
         ])
         
     }
